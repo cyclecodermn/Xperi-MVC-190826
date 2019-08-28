e38f4a6b949ae3540d481c32b71b17697d486fab
Steven Malikowski <cyclecoder@outlook.com>
2019-08-28 15:24:08 -0500

    Added field _repo in tests, removed several repo instanciations in methods

--- Xperi-MVC-190826.tests/MockRepoTests.cs
+++ Xperi-MVC-190826.tests/MockRepoTests.cs
@@ -12,7 +12,7 @@ namespace Xperi_MVC_190826.tests
     public class StaticMockRepoTests
     {
         //I don't know why the fields below do not work, but I want to figure it out, someday.
-        MockRepo _repo = new MockRepo();
+        private static MockRepo _repo = new MockRepo();
         private List<ToDoItem> _ToDos = new List<ToDoItem>();
 
         [TestFixture]
@@ -21,30 +21,26 @@ namespace Xperi_MVC_190826.tests
             [Test]
             public void CanLoadToDos()
             {
-                MockRepo repo = new MockRepo();
                 List<ToDoItem> ToDos = new List<ToDoItem>();
-                ToDos = repo.GetAll().ToList();
+                ToDos = _repo.GetAll().ToList();
                 Assert.AreEqual(3, ToDos.Count());
             }
 
             [Test]
             public void FoundTodosByName()
             {
-                MockRepo repo = new MockRepo();
                 List<ToDoItem> ToDos = new List<ToDoItem>();
 
-                ToDos = repo.GetByName("home").ToList();
+                ToDos = _repo.GetByName("home").ToList();
                 Assert.AreEqual(2, ToDos.Count());
             }
 
             [Test]
             public void FoundbyId()
             {
-                MockRepo repo = new MockRepo();
-
-                ToDoItem FirstToDo = repo.GetById(0);
-                ToDoItem SecondToDo = repo.GetById(1);
-                ToDoItem ThirdToDo = repo.GetById(2);
+                ToDoItem FirstToDo = _repo.GetById(0);
+                ToDoItem SecondToDo = _repo.GetById(1);
+                ToDoItem ThirdToDo = _repo.GetById(2);
 
                 Assert.AreEqual ("Go for a bike ride near home.", FirstToDo.Name);
                 Assert.AreEqual("Go for a bike ride far from home.", SecondToDo.Name);
@@ -56,27 +52,24 @@ namespace Xperi_MVC_190826.tests
             {
                 AddNewTodoForTesting();
 
-                MockRepo repo = new MockRepo();
                 ToDoItem newestToDo = new ToDoItem();
-                newestToDo = repo.GetById(3);
+                newestToDo = _repo.GetById(3);
 
                 Assert.AreEqual("Fix code.", newestToDo.Name);
-                repo.Delete(3);
+                _repo.Delete(3);
             }
             [Test]
             public void CanUpdateTodo()
             {
                 AddNewTodoForTesting();
 
-                MockRepo repo = new MockRepo();
-                
                 ToDoItem updatedToDo = new ToDoItem();
-                updatedToDo = repo.GetById(3);
+                updatedToDo = _repo.GetById(3);
 
                 updatedToDo.Name = "New name.";
 
                 Assert.AreEqual("New name.", updatedToDo.Name);
-                repo.Delete(3);
+                _repo.Delete(3);
             }
 
             public void AddNewTodoForTesting()
@@ -87,7 +80,7 @@ namespace Xperi_MVC_190826.tests
                 newToDo.Note = "Some note";
 
                 MockRepo repo = new MockRepo();
-                repo.Create(newToDo);
+                _repo.Create(newToDo);
 
             }
         }
