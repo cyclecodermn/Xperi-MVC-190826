af73a1ef66882e876fa14fd559c0bb1edf489b63
Steven Malikowski <cyclecoder@outlook.com>
2019-08-30 17:28:12 -0500

    Started adding Dapper, not working yet.

---
+++ Xperi-MVC-190826.tests/DapperRepoTests.cs
@@ -0,0 +1,92 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xperi_MVC.data.Models;
+using Xperi_MVC.data.Repos;
+
+namespace Xperi_MVC_190826.tests
+{
+    public class DapperRepoTests
+    {
+        private static DapperRepo _repo = new DapperRepo();
+        static DapperRepoTests()
+        {
+            DapperRepo _repo = new DapperRepo();
+        }
+        //I don't know why the field below doesn't work, but I want to figure it out, someday.
+        private List<ToDoItem> _ToDos = new List<ToDoItem>();
+
+        [TestFixture]
+        public class DapperRepoTest
+        {
+            [Test]
+            public void CanLoadToDos()
+            {
+                DapperRepo repo = new DapperRepo();
+                List<ToDoItem> ToDos = new List<ToDoItem>();
+                ToDos = repo.GetAll().ToList();
+                Assert.AreEqual(3, ToDos.Count());
+            }
+
+            [Test]
+            public void FoundTodosByName()
+            {
+                List<ToDoItem> ToDos = new List<ToDoItem>();
+
+                ToDos = _repo.GetByName("home").ToList();
+                Assert.AreEqual(2, ToDos.Count());
+            }
+
+            [Test]
+            public void FoundbyId()
+            {
+                ToDoItem FirstToDo = _repo.GetById(0);
+                ToDoItem SecondToDo = _repo.GetById(1);
+                ToDoItem ThirdToDo = _repo.GetById(2);
+
+                Assert.AreEqual ("Go for a bike ride near home.", FirstToDo.Name);
+                Assert.AreEqual("Go for a bike ride far from home.", SecondToDo.Name);
+                Assert.AreEqual("Oil bike chain", ThirdToDo.Name);
+
+            }
+            [Test]
+            public void CanAddTodo()
+            {
+                AddNewTodoForTesting();
+
+                ToDoItem newestToDo = new ToDoItem();
+                newestToDo = _repo.GetById(3);
+
+                Assert.AreEqual("Fix code.", newestToDo.Name);
+                _repo.Delete(3);
+            }
+            [Test]
+            public void CanUpdateTodo()
+            {
+                AddNewTodoForTesting();
+
+                ToDoItem updatedToDo = new ToDoItem();
+                updatedToDo = _repo.GetById(3);
+
+                updatedToDo.Name = "New name.";
+
+                Assert.AreEqual("New name.", updatedToDo.Name);
+                _repo.Delete(3);
+            }
+
+            public void AddNewTodoForTesting()
+            {
+                ToDoItem newToDo = new ToDoItem();
+                newToDo.Name = "Fix code.";
+                newToDo.Completed = true;
+                newToDo.Note = "Some note";
+
+                MockRepo repo = new MockRepo();
+                _repo.Create(newToDo);
+            }
+        }
+    }
+}
---
+++ Xperi-MVC.data/Repos/DapperRepo.cs
@@ -0,0 +1,81 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xperi_MVC.data.Models;
+
+namespace Xperi_MVC.data.Repos
+{
+    public class DapperRepo
+    {
+        private static List<ToDoItem> _ToDos;
+
+        static DapperRepo()
+        {
+
+            _ToDos = new List<ToDoItem>();
+
+            var _cn = ConfigurationManager.ConnectionStrings["ToDoDb"].ConnectionString.ToString();
+
+        }
+
+            public IEnumerable<ToDoItem> GetAll()
+        {
+            //_cn= "Server = myServerAddress; Database = ToDoXperi; Trusted_Connection = True;";
+                using (var cn = new SqlConnection())
+                {
+                //cn.ConnectionString = ConfigurationManager
+                //    .ConnectionStrings["ToDoDb"]
+                //    .ConnectionString;
+
+                cn.ConnectionString = "Server = myServerAddress; Database = ToDoXperi; Trusted_Connection = True;";
+
+                return cn.Query<ToDoItem>("MovieSelectAll",
+                        commandType: CommandType.StoredProcedure);
+                }
+
+
+        }
+
+                public  ToDoItem GetById(int Id)
+        {
+            return _ToDos.FirstOrDefault(d => d.Id == Id);
+        }
+
+        public  void Create(ToDoItem newToDo)
+        {
+            if (_ToDos.Any())
+            {
+                newToDo.Id = _ToDos.Max(d => d.Id) + 1;
+            }
+            else
+            {
+                newToDo.Id = 0;
+            }
+
+            _ToDos.Add(newToDo);
+        }
+
+
+        public  void Update(ToDoItem updatedToDo)
+        {
+            _ToDos.RemoveAll(d => d.Id == updatedToDo.Id);
+            _ToDos.Add(updatedToDo);
+        }
+
+        public  void Delete(int Id)
+        {
+            _ToDos.RemoveAll(d => d.Id == Id);
+        }
+
+        public IEnumerable<ToDoItem> GetByName(string term)
+        {
+            return _ToDos.Where(d => d.Name.Contains(term));
+        }
+    }
+}
