2c251a2f1b75a2c9eee54baf4709f23854c1b061
Steven Malikowski <cyclecoder@outlook.com>
2019-09-18 15:34:58 -0500

    Added test to update test, but it only works after re-running sproks. I need to add an init.

---
+++ Xperi-MVC-190826/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Xperi_MVC.data.Models;
+using Xperi_MVC.data.Repos;
+
+namespace Xperi_MVC_190826.Controllers
+{
+    public class HomeController: Controller
+    {
+        private static DapperRepo _repo = new DapperRepo();
+
+        public ActionResult ShowAllToDos()
+        {
+            List<ToDoTableRow> AllToDos = new List<ToDoTableRow>();
+            AllToDos = _repo.GetAll().ToList();
+
+            return View(AllToDos);
+        }
+
+        public ActionResult Index()
+        {
+            List<ToDoTableRow> AllToDos = new List<ToDoTableRow>();
+            AllToDos = _repo.GetAll().ToList();
+
+            return View(AllToDos);
+        }
+    }
+
+
+}
